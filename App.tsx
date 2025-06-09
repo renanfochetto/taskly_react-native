9f664f79e2bb1a7e8e7940f2769814e00f4ff8ac
rnn <145611850+renanfochetto@users.noreply.github.com>
2025-06-09 20:02:50 -0300

    Add theme support and delete confirmation in App component
    - Introduced a new theme file with color definitions.
    - Implemented a delete confirmation alert in the App component.
    - Updated styles for better layout and user experience.
    - Added ESLint and Prettier for code quality and formatting.
    - Updated .gitignore to include notes.txt.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  Pressable,
+  Alert
+} from "react-native";
+import {theme} from "./theme";
+import {log} from "expo/build/devtools/logger";
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert(
+      "Are you sure you want to delete this item?",
+      "It will be gone for good",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("Ok, deleting item..."),
+          style: "destructive",
+        },
+        {
+          text: "Cancel",
+          style: "cancel",
+        }
+      ])
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => handleDelete()}
+          activeOpacity={0.8}
+        >
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +48,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
   },
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#1a759f",
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  itemText: {
+    fontSize: 18,
+    fontWeight: "200"
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
+  }
 });
---
+++ theme.ts
@@ -0,0 +1,5 @@
+export const theme = {
+  colorCerulean: "#1a759f",
+  colorWhite: "#ffffff",
+  colorBlack: "#000000"
+};
